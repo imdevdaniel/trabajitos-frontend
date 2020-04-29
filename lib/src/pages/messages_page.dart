63b235c8db6725c845fd8d70a55f2176a8d87326
doncanzsa <doncanzsa@gmail.com>
2020-04-28 20:28:06 -0600

    add messages to page messages

--- lib/src/pages/messages_page.dart
+++ lib/src/pages/messages_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+// import 'package:flutter/services.dart' show rootBundle;
 
 class MessagesPage extends StatefulWidget {
 
@@ -10,11 +10,81 @@ class MessagesPage extends StatefulWidget {
 
 class _MessagesPageState extends State<MessagesPage> {
 
+  final opciones = [
+    {
+      'name': 'Jose Carlos Tobar',
+      'msj':  'Ya estoy en camino, llegare en breve',
+      'img':  '125'
+    },
+    {
+      'name': 'Victor Alexander Perez',
+      'msj':  'De momento ese seria el costo',
+      'img':  '84'
+    },
+    {
+      'name': 'Abisai Melendez',
+      'msj':  'Me puede brindar los requisitos?',
+      'img':  '32'
+    },
+    {
+      'name': 'Daniel Pacheco',
+      'msj':  'Solamente seria diseño estatico',
+      'img':  '71'
+    },
+    {
+      'name': 'Didier Alvarez',
+      'msj':  'Mire, de momento no me hace tanta falta',
+      'img':  '29'
+    },
+    {
+      'name': 'Oscar Marquez',
+      'msj':  'Estamos pendientes...',
+      'img':  '100'
+    },
+    {
+      'name': 'Marvin Quebedo',
+      'msj':  'Desea solamente instalacion o con materiales de construccion.',
+      'img':  '161'
+    },
+    {
+      'name': 'Ana Vilma Herrera',
+      'msj':  'Le puedo hacer una rebaja si son todos',
+      'img':  '152'
+    },
+    {
+      'name': 'Maria Transito',
+      'msj':  'Lo siento, ropa de hombre no reparo',
+      'img':  '158'
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Messages page content goes here'),
+    return ListView(
+      children: _crearItemsCorto(),
     );
+  }
+
+  List<Widget> _crearItemsCorto() {
+    
+    // rootBundle.loadString('assets/json/messages.json').then((data) {
+    //   print(data);
+    // });
+
+    return opciones.map((var opt) {
+      return Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(opt['name']),
+            subtitle: Text(opt['msj']),
+            leading: Image.network('https://i.picsum.photos/id/' + opt['img'] + '/100/100.jpg'),
+            trailing: Icon(Icons.more_horiz),
+            onTap: () {},
+          ),
+          Divider()
+        ],
+      );
+    }).toList();
   }
 
 }
