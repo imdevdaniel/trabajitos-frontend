341a078c326c57803a4901d36b88baa90dc4e6fb
melendezabisai <melendezabisai1@gmail.com>
2020-04-29 11:33:45 -0600

    modificacion de help_page coloque un ScrollView

--- lib/src/pages/password_reset_page.dart
+++ lib/src/pages/password_reset_page.dart
@@ -24,11 +24,11 @@ final Color co = new Color(0xFF448AFF);
       ),
 
 
-     body: SingleChildScrollView( 
+     body: Center( 
           
         child: Column(
          
-         
+          mainAxisAlignment: MainAxisAlignment.center,
 
           children: <Widget>[
             
