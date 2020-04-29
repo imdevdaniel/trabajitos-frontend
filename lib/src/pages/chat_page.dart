95ce80c7515415a024a80a34ea05739caa058c89
melendezabisai <melendezabisai1@gmail.com>
2020-04-28 21:33:18 -0600

    modificacion del texto contenido en la pagina chat_page

--- lib/src/pages/chat_page.dart
+++ lib/src/pages/chat_page.dart
@@ -17,7 +17,7 @@ class _ChatPageState extends State<ChatPage> {
       appBar: AppBar(title: Text('José (Electricista)')),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Text('Chats go here'),
+          child: Text('Jodanse'),
         ),
       ),
     );
